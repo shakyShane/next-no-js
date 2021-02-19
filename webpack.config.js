3d4680ef09d0136863f0269bfa2a204bf32f1f70
Shane Osbourne <shane.osbourne8@gmail.com>
2021-02-19 22:17:04 +0000

    fix duplicated imports

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,17 @@ module.exports = () => {
                     name: "modfed-entry",
 
                     // list of shared modules from shell
-                    shared: ["react", "react-dom"],
+                    shared: {
+                        react: {
+                            import: "react", // the "react" package will be used a provided and fallback module
+                            shareKey: "react", // under this name the shared module will be placed in the share scope
+                            shareScope: "default", // share scope with this name will be used
+                            singleton: true, // only a single version of the shared module is allowed
+                        },
+                        "react-dom": {
+                            singleton: true, // only a single version of the shared module is allowed
+                        },
+                    }
                 }),
                 new webpack.container.ModuleFederationPlugin({
                     name: "counter",
