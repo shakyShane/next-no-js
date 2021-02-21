231274af5c072b9f4c041e0a5e58d4b64ae1784d
Shane Osbourne <shane.osbourne8@gmail.com>
2021-02-21 21:28:41 +0000

    small images

--- data/products.ts
+++ data/products.ts
@@ -10,18 +10,18 @@ export const products: Product[] = [
         sku: "0123",
         price: "123.99",
         name: "Classic Watch",
-        image: "https://source.unsplash.com/400x280?wrist-watch",
+        image: "/watch-1.webp",
     },
     {
         sku: "456",
         price: "99.99",
         name: "Old Watch",
-        image: "https://source.unsplash.com/400x280?watch",
+        image: "/watch-2.webp",
     },
     {
         sku: "78993",
         price: "57.99",
         name: "Fossil Watch",
-        image: "https://source.unsplash.com/400x280?gold-watch",
+        image: "/watch-3.webp",
     },
 ];
