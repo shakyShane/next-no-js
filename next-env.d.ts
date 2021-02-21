404711cf951ef081c43374124bf93c98db137ad8
Shane Osbourne <shane.osbourne8@gmail.com>
2021-02-21 17:30:56 +0000

    frame

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,18 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+import { HTMLAttributes } from "react";
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            "turbo-frame": any; // The 'any' just for testing purposes
+        }
+    }
+}
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        "turbo-frame": HTMLAttributes;
+    }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,11 +10,13 @@ function MyApp({ Component, pageProps }) {
             <Loader modfedType={"preact"} modfedComponent={"Minicart"}>
                 <Minicart />
             </Loader>
-            <main className="my-8">
-                <div className="container mx-auto px-6">
-                    <Component {...pageProps} />
-                </div>
-            </main>
+            <turbo-frame id="message_1" target="_top">
+                <main className="my-8">
+                    <div className="container mx-auto px-6">
+                        <Component {...pageProps} />
+                    </div>
+                </main>
+            </turbo-frame>
             <footer className="bg-gray-200">
                 <div className="container mx-auto px-6 py-3 flex justify-between items-center">
                     <a href="#" className="text-xl font-bold text-gray-500 hover:text-gray-400">
