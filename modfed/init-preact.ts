7eae9a19dd2f95ceeb60507cf2800bd0b80a892f
Shane Osbourne <shane.osbourne8@gmail.com>
2021-02-21 17:23:04 +0000

    preventing duplicated hydrations

--- modfed/init-preact.ts
+++ modfed/init-preact.ts
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export function hydrate(item: HTMLElement) {
-    console.log("hydrating", item);
-    const { modfedId, modfedComponent, modfedSelf } = item.dataset;
+    const { modfedId, modfedComponent, modfedSelf, modfedHydrated } = item.dataset;
+    console.log("prev", modfedComponent, modfedHydrated);
+    if (modfedHydrated) {
+        console.log("skipping item hydration");
+        return;
+    }
     const data = item.parentElement.querySelector(`[data-modfed-data]`);
     const parsed = JSON.parse(data?.textContent ?? "null");
 
@@ -11,6 +15,8 @@ export function hydrate(item: HTMLElement) {
         if (!mod.default) {
             throw new Error(`"default" missing in module ${modfedComponent}`);
         }
+        console.log("hydrating", item);
+        item.setAttribute("data-modfed-hydrated", "true");
         if (modfedSelf) {
             console.log("using parent as root");
             ReactDOM.render(React.createElement(mod.default, parsed), item.parentElement, item);
