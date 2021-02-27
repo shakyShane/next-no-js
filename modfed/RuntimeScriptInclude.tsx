58ca90a4bf1615c79eb56bb9e9a65dfe36de3baf
Shane Osbourne <shane.osbourne8@gmail.com>
2021-02-27 21:38:22 +0000

    bootstrap

--- modfed/RuntimeScriptInclude.tsx
+++ modfed/RuntimeScriptInclude.tsx
@@ -1,63 +1,33 @@
-import { join } from "path";
-import { existsSync, readFileSync } from "fs";
-
-let manifest;
-let bootstrap;
-let CWD = process.cwd();
-let NEXT_DIR = existsSync(join(CWD, ".next"));
-let MANIFEST_EXISTS = existsSync(join(CWD, "public", "modfed-entry.json"));
-
-if (process.env.NODE_ENV === "production") {
-    try {
-        const str = readFileSync(join(CWD, "public", "modfed-entry.json"), "utf8");
-        manifest = JSON.parse(str);
-        const entry = manifest.children.find((child) => child.name === "modfed-entry");
-        bootstrap = entry.assetsByChunkName.bootstrap[0];
-        if (!bootstrap) {
-            console.error("could not load bootstrap");
-        }
-    } catch (e) {
-        console.error("could not read modfed manifest");
-    }
-}
-
 export function RuntimeScriptInclude(props: { html: string }) {
-    if (process.env.NODE_ENV === "production") {
-        if (!bootstrap) {
-            return (
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: `<!-- missing bootstrap, cwd: ${CWD}, .next exists: ${String(
-                            NEXT_DIR
-                        )}, manifest exists: ${String(MANIFEST_EXISTS)} -->`,
-                    }}
-                />
-            );
-        } else {
-            if (!props.html.includes("data-modfed-kind")) {
-                return <div dangerouslySetInnerHTML={{ __html: `<!-- no JS components found -->` }} />;
-            } else {
-                const runtimes = [];
-                if (props.html.includes(`data-modfed-kind="vanilla"`)) {
-                    runtimes.push("vanilla");
-                }
-                if (props.html.includes(`data-modfed-kind="preact"`)) {
-                    runtimes.push("preact");
-                }
-                const json = { runtimes };
-                return (
-                    <>
-                        <script
-                            type={"text/json"}
-                            dangerouslySetInnerHTML={{ __html: JSON.stringify(json).replace(/</g, "\\u003c") }}
-                            id="bootstrap"
-                        />
-                        <script src={`/_next/static/chunks/modfed/${bootstrap}`} />
-                    </>
-                );
-            }
-        }
+    if (process.env.NODE_ENV !== "production") {
+        console.log("not adding JS (non-prod)");
+        return null;
     }
-    console.log("not adding JS");
-    return null;
+
+    if (!process.env.BOOTSTRAP) {
+        return <div dangerouslySetInnerHTML={{ __html: `<!-- missing bootstrap -->` }} />;
+    }
+
+    if (!props.html.includes("data-modfed-kind")) {
+        return <div dangerouslySetInnerHTML={{ __html: `<!-- no JS components found -->` }} />;
+    }
+
+    const runtimes = [];
+    if (props.html.includes(`data-modfed-kind="vanilla"`)) {
+        runtimes.push("vanilla");
+    }
+    if (props.html.includes(`data-modfed-kind="preact"`)) {
+        runtimes.push("preact");
+    }
+    const json = { runtimes };
+    return (
+        <>
+            <script
+                type={"text/json"}
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(json).replace(/</g, "\\u003c") }}
+                id="bootstrap"
+            />
+            <script src={`/_next/static/chunks/modfed/${process.env.BOOTSTRAP}`} />
+        </>
+    );
 }
